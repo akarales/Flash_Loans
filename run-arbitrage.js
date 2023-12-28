173ad3bcc28b35fd5a71e732451679fbc44209cc
akarales <karales@gmail.com>
2023-12-28 02:20:19 -0500

    run-arbitrage.js :: updated

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -3,12 +3,13 @@ require('dotenv').config()
 const infuraUrl = process.env.INFURA_URL;
 const abis = require('./abis')
 const { mainnet: addresses } = require('./addresses')
+
 if (!infuraUrl) {
   console.error('INFURA_URL is not defined in your .env file');
   process.exit(1);
 }
 
-const {Web3} = require("web3");
+const {Web3, eth} = require("web3");
 
 // Configure reconnect options for WebsocketProvider
 const websocketOptions = {
@@ -30,10 +31,17 @@ const kyber = new web3.eth.Contract(
     addresses.kyber.kyberNetworkProxy
 );
 
-// const AMOUNT_ETH = 100;
-// const RECENT_ETH_PRICE = 230;
-// const AMOUNT_ETH_WEI = web3.utils.toWei(AMOUNT_ETH.toString());
-// const AMOUNT_DIA_WEI = web3.utils.toWei((AMOUNT_ETH * RECENT_ETH_PRICE).toString());
+const AMOUNT_ETH = 100;
+// update eth price regularly
+const RECENT_ETH_PRICE = 2391;
+const AMOUNT_ETH_WEI = web3.utils.toWei(AMOUNT_ETH.toString(), 'ether');
+const AMOUNT_DAI_WEI = web3.utils.toWei((AMOUNT_ETH * RECENT_ETH_PRICE).toString(), 'ether');
+
+// Check if AMOUNT_ETH and RECENT_ETH_PRICE are defined and are numbers
+if (typeof AMOUNT_ETH !== 'number' || typeof RECENT_ETH_PRICE !== 'number') {
+    console.error('AMOUNT_ETH and/or RECENT_ETH_PRICE are not defined correctly.');
+    process.exit(1);
+  }
 
 async function subscribeToNewBlocks() {
     try {
@@ -57,9 +65,36 @@ function handleConnected(subscriptionId) {
 }
 
 // Event listener that logs the received block header data
-function handleNewBlock(blockHeader) {
+async function handleNewBlock(blockHeader) {
     // console.log(blockHeader);
     console.log(`Block number: ${blockHeader.number}`);
+
+    const kyberResults = await Promise.all([
+        kyber
+          .methods
+          .getExpectedRate(
+            addresses.tokens.dai, 
+            '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 
+            AMOUNT_DAI_WEI
+          ) 
+          .call(),
+        kyber
+          .methods
+          .getExpectedRate(
+            '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 
+            addresses.tokens.dai, 
+            AMOUNT_ETH_WEI
+          ) 
+          .call()
+    ]);
+    // console.log(kyberResults);
+    const kyberRates = {
+        // buy: parseFloat(1 / Number(kyberResults[0].expectedRate) / (10 ** 18)),
+        buy: parseFloat(1 / (Number(kyberResults[0].expectedRate) / (10 ** 18))),
+        sell: Number(kyberResults[1].expectedRate) / (10 ** 18)
+    };
+    console.log('Kyber ETH/DAI');
+    console.log(kyberRates);
 }
 
 // Event listener that logs any errors that occur
